80ac5d17de795c084c085961c60e5e93bc1e83bd
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-05-15 13:05:17 +0200

    feat: implemented searchbar to book index

--- versante-nord/src/main/java/org/lessons/java/versante_nord/repository/BookRepository.java
+++ versante-nord/src/main/java/org/lessons/java/versante_nord/repository/BookRepository.java
@@ -1,10 +1,12 @@
 package org.lessons.java.versante_nord.repository;
 
+import java.util.List;
+
 import org.lessons.java.versante_nord.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    // Non sono necessari metodi aggiuntivi per ora
-    // JpaRepository fornisce già i metodi CRUD di base
+
+    public List<Book> findByTitoloContainingIgnoreCaseOrAutoreContainingIgnoreCase(String title, String autore);
     
 }
